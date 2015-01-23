a1007d4e40f524292a4acb3c569840cf9041a5e5
Sandra Bardot <sandra.bardot@orange.fr>
2015-01-23 09:31:05 +0100

    contructor modified

--- src/recogestureherger/HergerUI.java
+++ src/recogestureherger/HergerUI.java
@@ -26,11 +26,10 @@ public class HergerUI extends javax.swing.JFrame {
     /**
      * Creates new form NewJFrame
      */
-    public HergerUI() {
+    public HergerUI(String adress, int x, int y, int with, int height) {
         path = new ArrayList<>();
         myRecognizer = new OneDollarRecognizer();
         myRecognizer.setup();
-
         initComponents();
     }
 
@@ -43,63 +42,21 @@ public class HergerUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jPanel1.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
-            public void mouseDragged(java.awt.event.MouseEvent evt) {
-                jPanel1MouseDragged(evt);
-            }
-        });
-        jPanel1.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseReleased(java.awt.event.MouseEvent evt) {
-                jPanel1MouseReleased(evt);
-            }
-        });
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGap(0, 300, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jPanel1MouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel1MouseDragged
-        Point newPoint = new Point(evt.getX(), evt.getY());
-        
-        path.add(newPoint);
-    }//GEN-LAST:event_jPanel1MouseDragged
-
-    private void jPanel1MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel1MouseReleased
-        System.out.println("Result : " + path.size());
-
-        // do the algorithm
-        OneDollarRecognizer.Result form = myRecognizer.recognizer.recognize(path);
-        System.out.println("Result of recognition is : Name(" + form.Name
-                + ") | Score(" + form.Score + ") | Ratio(" + form.Ratio + ")");
-
-        // clear the path
-        path.clear();
-    }//GEN-LAST:event_jPanel1MouseReleased
 
     /**
      * @param args the command line arguments
@@ -132,13 +89,12 @@ public class HergerUI extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new HergerUI().setVisible(true);
+                new HergerUI("",0,0,0,0).setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 
     private void doDrawing(Graphics g) {
@@ -163,7 +119,7 @@ public class HergerUI extends javax.swing.JFrame {
         }
     }
 
-    /*@Override
+   /* @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         doDrawing(g);
